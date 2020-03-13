a33d2ad122ef1c426e2e2bf23a68a3e50e71ee7f
Toadsmen77 <Toadsmen77@10.0.0.87>
2020-03-12 19:15:00 -0700

    Second push

--- HW7HeapADT/src/MaxHeapTest.java
+++ HW7HeapADT/src/MaxHeapTest.java
@@ -1,7 +1,34 @@
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class MaxHeapTest {
+	
     // homework
+	@Test
+	public void testNLogN() {
+		
+		int[][] testArrays = {
+				
+				{}, //empty
+				{60, 30, 15, 2, 7}, //already heapified, shouldn't change
+				
+				//not in heap form
+				{10, 1, 6, 7, 5, 8},
+				{1, 0, 5, 9, 4},
+				{2, 5, 8, 10},
+				
+				{1, 1, 1, 1, 2}, //top-priority in last
+				{1, 1, 2, 1, 1}, //top-priority in middle
+				{1, 1, 1, 1,}, //all same element
+				{1} //single element
+		};
+		
+		
+		
+		
+	}
+	
 	
 	
 	
